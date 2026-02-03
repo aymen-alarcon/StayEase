c4c023cbd8dbc3f2e0b13b22134a9e45fde40a08
ghizlane-bouaddi <gizlanebouaddi378@gmail.com>
2026-02-03 14:51:05 +0100

    Initial Laravel 12 setup

--- database/migrations/2026_02_02_132959_create_tag_table.php
+++ database/migrations/2026_02_02_132959_create_tag_table.php
@@ -13,8 +13,8 @@ return new class extends Migration
     {
         Schema::create('tags', function (Blueprint $table) {
             $table->id();
-            $table->string("name");
-            $table->string("description");
+            $table->string('name');
+            $table->string('slug')->unique();
             $table->timestamps();
         });
     }
