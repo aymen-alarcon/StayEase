95ad36980d07390a4795fec1c24de02f50ee3d76
ghizlane-bouaddi <gizlanebouaddi378@gmail.com>
2026-02-04 14:27:01 +0100

    migration

--- app/Models/Room.php
+++ app/Models/Room.php
@@ -10,7 +10,7 @@ class Room extends Model
     protected $fillable =['hotel_id','number','price_per_night','capacity','description'];
 
     public function tags(){
-return $this->belongsToMany(Tag::class, 'rooms_tags');
+      return $this->belongsToMany(Tag::class, 'rooms_tags');
 }
 public function properties(){
 return $this->belongsToMany(Property::class, 'rooms_properties');
