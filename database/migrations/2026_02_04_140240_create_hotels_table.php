f45768f9cfc59930b3a398784822238758a142c8
oughlane <mohammedoughlane72@gmail.com>
2026-02-05 09:29:26 +0100

    fixer des problmes

--- database/migrations/2026_02_04_140240_create_hotels_table.php
+++ database/migrations/2026_02_04_140240_create_hotels_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('rooms_properties', function (Blueprint $table) {
+        Schema::create('hotels', function (Blueprint $table) {
             $table->id();
 
              $table->string("name");
