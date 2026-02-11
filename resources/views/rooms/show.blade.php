cdbc867b685133b62bf984e13d22e5002ab16d1d
ghizlane-bouaddi <gizlanebouaddi378@gmail.com>
2026-02-11 09:21:03 +0100

    ui create et update

--- resources/views/rooms/show.blade.php
+++ resources/views/rooms/show.blade.php
@@ -9,6 +9,50 @@
 </head>
 <body>
 
+
+
+    <div class="container-fluid bg-dark px-0">
+        <div class="row gx-0">
+            <div class="col-lg-3 bg-dark d-none d-lg-block">
+                <a href="index.html"
+                    class="navbar-brand w-100 h-100 m-0 p-0 d-flex align-items-center justify-content-center">
+                    <h1 class="m-0 text-primary text-uppercase">Hotelier</h1>
+                </a>
+            </div>
+            <div class="col-lg-9">
+                <nav class="navbar navbar-expand-lg bg-dark navbar-dark p-3 p-lg-0">
+                    <button type="button" class="navbar-toggler" data-bs-toggle="collapse"
+                        data-bs-target="#navbarCollapse">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse justify-content-between" id="navbarCollapse">
+                        <div class="navbar-nav mr-auto py-0">
+                            <a href="index.html" class="nav-item nav-link active">Home</a>
+                            <a href="{{ route('rooms.index') }}" class="nav-item nav-link">Rooms</a>
+                            @guest
+                                <a href="/signup" class="nav-item nav-link">SignUp</a>
+                                <a href="/login" class="nav-item nav-link">Login</a>
+                            @endguest
+
+                            @auth
+                                <form action="/logout" method="post" class="d-flex align-items-center">
+                                @csrf
+                                @method('DELETE')
+                                    <button type="submit" class="nav-item nav-link btn btn-link p-0 m-0">
+                                        Log Out
+                                    </button>
+                                </form>
+                            @endauth
+                        </div>
+                    </div>
+                </nav>
+            </div>
+        </div>
+    </div>
+
+
+
+
 <div class="container py-5">
     <div class="row">
         <div class="col-lg-8">
